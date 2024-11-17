e39a783020bf3ed5adde524e8c3650273e602455
xingyu yao <xy2779@nyu.edu>
2024-11-17 14:36:38 -0500

    Update sketch.js

--- 0/sketch.js
+++ 0/sketch.js
@@ -4,46 +4,48 @@ let fft;
 let particles = [];
 
 function preload() {
-  song = loadSound('counting stars.mp3'); 
+  song = loadSound("counting stars.mp3");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  song.loop(); 
+  song.loop();
   amp = new p5.Amplitude();
   fft = new p5.FFT();
 }
 
 function draw() {
-  background(0, 50); 
+  background(0, 50);
 
-  let volume = amp.getLevel(); 
-  let spectrum = fft.analyze(); 
+  let volume = amp.getLevel();
+  let spectrum = fft.analyze();
 
-  let highFreq = spectrum.reduce((sum, val, idx) => idx > spectrum.length / 2 ? sum + val : sum, 0);
+  let highFreq = spectrum.reduce(
+    (sum, val, idx) => (idx > spectrum.length / 2 ? sum + val : sum),
+    0
+  );
 
-  // 根据音量和频率生成新粒子
+  // Generate new particles based on volume and frequency
   for (let i = 0; i < map(highFreq, 0, 1000, 1, 10); i++) {
     particles.push(new Particle(volume));
   }
 
-  // 更新和绘制粒子
+  // Updating and drawing particles
   for (let i = particles.length - 1; i >= 0; i--) {
     particles[i].update();
     particles[i].show();
     if (particles[i].isOffScreen()) {
-      particles.splice(i, 1); // 移除屏幕外的粒子
+      particles.splice(i, 1); // Remove off-screen particles
     }
   }
 }
 
-// 粒子类
 class Particle {
   constructor(volume) {
     this.pos = createVector(random(width), random(height));
     this.size = random(2, 5) + map(volume, 0, 0.5, 1, 30);
     this.alpha = map(volume, 0, 0.5, 0, 255); // 透明度随音量变化
-    this.color = color(255, 255, 255, this.alpha); 
+    this.color = color(255, 255, 255, this.alpha);
     this.lifespan = 255;
   }
 
